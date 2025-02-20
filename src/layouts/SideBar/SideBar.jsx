8d7fb2a40e53cb6cdfbecacf783227930a473851
JemLlanto <llantojem@gmail.com>
2025-02-20 11:00:48 +0800

    sidebar for admin

--- src/layouts/SideBar/SideBar.jsx
+++ src/layouts/SideBar/SideBar.jsx
@@ -3,7 +3,7 @@ import { Link, useLocation } from "react-router-dom";
 import "boxicons/css/boxicons.min.css";
 import Footer from "../../components/Footer/Footer";
 
-const SideBar = () => {
+const SideBar = ({ user }) => {
   const [showSidebar, setShowSidebar] = useState(true);
   const location = useLocation(); // Get current route for active menu
 
@@ -32,39 +32,79 @@ const SideBar = () => {
           </button>
         </div>
         <div className=" listGroup">
-          <ul className="sideBar-list list-unstyled">
-            <li
-              className={`list-group-items ${
-                location.pathname === "/Home" ? "active" : ""
-              }`}
-            >
-              <Link to="/Home" className="d-flex">
-                <i className="bx bx-home"></i>
-                <p className="m-0"> Homepage</p>
-              </Link>
-            </li>
-            <li
-              className={`list-group-items ${
-                location.pathname === "/RequestDocuments" ? "active" : ""
-              }`}
-            >
-              <Link to="/RequestDocuments">
-                <i className="bx bx-file"></i>
-                <p className="m-0"> Request Documents</p>
-              </Link>
-            </li>
+          {user.isAdmin ? (
+            <>
+              <ul className="sideBar-list list-unstyled">
+                <li
+                  className={`list-group-items ${
+                    location.pathname === "/Home" ? "active" : ""
+                  }`}
+                >
+                  <Link to="/Home" className="d-flex">
+                    <i className="bx bx-home"></i>
+                    <p className="m-0"> Admin Homepage</p>
+                  </Link>
+                </li>
+                <li
+                  className={`list-group-items ${
+                    location.pathname === "/RequestDocuments" ? "active" : ""
+                  }`}
+                >
+                  <Link to="/RequestDocuments">
+                    <i className="bx bx-file"></i>
+                    <p className="m-0"> Request Documents</p>
+                  </Link>
+                </li>
 
-            <li
-              className={`list-group-items ${
-                location.pathname === "/About" ? "active" : ""
-              }`}
-            >
-              <Link to="/About">
-                <i className="bx bx-info-circle"></i>
-                <p className="m-0"> About Us</p>
-              </Link>
-            </li>
-          </ul>
+                <li
+                  className={`list-group-items ${
+                    location.pathname === "/About" ? "active" : ""
+                  }`}
+                >
+                  <Link to="/About">
+                    <i className="bx bx-info-circle"></i>
+                    <p className="m-0"> About Us</p>
+                  </Link>
+                </li>
+              </ul>
+            </>
+          ) : (
+            <>
+              <ul className="sideBar-list list-unstyled">
+                <li
+                  className={`list-group-items ${
+                    location.pathname === "/Home" ? "active" : ""
+                  }`}
+                >
+                  <Link to="/Home" className="d-flex">
+                    <i className="bx bx-home"></i>
+                    <p className="m-0"> Homepage</p>
+                  </Link>
+                </li>
+                <li
+                  className={`list-group-items ${
+                    location.pathname === "/RequestDocuments" ? "active" : ""
+                  }`}
+                >
+                  <Link to="/RequestDocuments">
+                    <i className="bx bx-file"></i>
+                    <p className="m-0"> Request Documents</p>
+                  </Link>
+                </li>
+
+                <li
+                  className={`list-group-items ${
+                    location.pathname === "/About" ? "active" : ""
+                  }`}
+                >
+                  <Link to="/About">
+                    <i className="bx bx-info-circle"></i>
+                    <p className="m-0"> About Us</p>
+                  </Link>
+                </li>
+              </ul>
+            </>
+          )}
         </div>
       </div>
       <div className="">
