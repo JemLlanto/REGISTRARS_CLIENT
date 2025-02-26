29d457051a09bccd4ffc2129c1821c21edb5c6f8
JMsallao <sallaojohnmark2772@gmail.com>
2025-02-26 17:36:37 +0800

    about

--- src/pages/user/About.jsx
+++ src/pages/user/About.jsx
@@ -14,42 +14,74 @@ export default function About() {
           About Us:
         </h5>
       </div>
-      <div className="d-flex align-items-center justify-content-center w-100 h-50 bg-white shadow-sm rounded-2 flex-column mt-5">
-        <div className="">
-          <img
-            src="/OfficeLogo.png"
-            alt="Registrar Logo"
-            style={{ width: "20rem" }}
-          />
+      <div
+        className="d-flex justify-content-between gap-3"
+        style={{ maxHeight: "650px" }}
+      >
+        <div className="d-flex align-items-center justify-content-start w-100  bg-success shadow-sm rounded-2 flex-column mt-5 p-5">
+          <div className="m-0">
+            <h3 className="m-0 fw-bold text-white">QUALITY POLICY</h3>
+          </div>
+          <div className="mt-3">
+            <p className="m-0 text-center text-white">
+              We Commit to the highest standards of education, value our
+              stakeholders, Strive for continual improvement of our products and
+              services, and Uphold the University’s tenets of Truth, Excellence,
+              and Service to produce globally competitive and morally upright
+              individuals.
+            </p>
+          </div>
         </div>
-        <div className="m-0 mt-3">
-          <h3>OFFICE HOURS</h3>
+        <div className="d-flex align-items-center justify-content-start w-100  bg-success shadow-sm rounded-2 flex-column mt-5 p-5">
+          <div className="m-0">
+            <h3 className="m-0 fw-bold text-white">VISION</h3>
+          </div>
+          <div className="mt-3">
+            <p className="m-0 text-center text-white">
+              The premier university in historic Cavite globally recognized for
+              excellence in character development, academics, research,
+              innovation, and sustainable community engagement.
+            </p>
+          </div>
         </div>
-        <div className="mt-3">
-          <h1>MONDAY - THURSDAY</h1>
-        </div>
-        <div>
-          <p>07:00 AM to 06:00 PM</p>
+        <div className="d-flex align-items-center justify-content-start w-100  bg-success shadow-sm rounded-2 flex-column mt-5 p-5">
+          <div className="m-0">
+            <h3 className="m-0 fw-bold text-white">MISSION</h3>
+          </div>
+          <div className="mt-3">
+            <p className="m-0 text-center text-white">
+              CAVITE STATE UNIVERSITY shall provide excellent, equitable and
+              relevant educational opportunities in the arts, sciences, and
+              technology through quality instruction and responsive research and
+              development activities. It shall produce professional skilled and
+              morally upright individuals for global competitiveness.
+            </p>
+          </div>
         </div>
       </div>
 
-      <div className="d-flex align-items-center justify-content-center w-100 h-50 bg-white shadow-sm rounded-2 flex-column mt-4">
-        <div className="">
-          <img
-            src="/OfficeLogo.png"
-            alt="Registrar Logo"
-            style={{ width: "20rem" }}
-          />
+      {/* <li
+        className=" w-100 list-unstyled mt-5"
+        style={{ height: "2px", backgroundColor: "var(--secondMain-color)" }}
+      ></li> */}
+
+      <div
+        className="core d-flex  justify-content-evenly w-100  shadow-sm rounded-2 p-5 mt-5"
+        style={{
+          backgroundColor: "var(--main-color)",
+        }}
+      >
+        <div className="m-0 ">
+          <h2 className=" fw-bold">TRUTH</h2>
         </div>
-        <div className="m-0 mt-3">
-          <h4>MANAGEMENT INFORMATION SYSTEMS</h4>
+        <div className="m-0 ">
+          <h2 className=" fw-bold">INTEGRITY</h2>
         </div>
-        <div className="mt-3 m-0">
-          <h1>ADDING, CHANGING, AND DROPPING OF SUBJECTS</h1>
-          <p className="text-center">SCHEDULE:</p>
+        <div className="m-0 ">
+          <h2 className=" fw-bold">EXCELLENCE</h2>
         </div>
-        <div className="mb-2">
-          <h5>CUT-OFF TIME: 4:00 PM</h5>
+        <div className="m-0">
+          <h2 className=" fw-bold">SERVICE</h2>
         </div>
       </div>
 
