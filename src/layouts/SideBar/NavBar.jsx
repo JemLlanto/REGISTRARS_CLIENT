e636dc788032e26ebb33614f6e4734aa62f3c03d
JMsallao <sallaojohnmark2772@gmail.com>
2025-02-26 17:36:43 +0800

    notif

--- src/layouts/SideBar/NavBar.jsx
+++ src/layouts/SideBar/NavBar.jsx
@@ -1,59 +1,91 @@
-import React from "react";
-import { Link, useLocation } from "react-router-dom";
-import { Dropdown, DropdownButton } from "react-bootstrap";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Dropdown, Button } from "react-bootstrap";
 import axios from "axios";
+import { IoNotificationsOutline } from "react-icons/io5";
 
 const NavBar = ({ user }) => {
+  const [notifCount, setNotifCount] = useState(3);
+  const [showNotif, setShowNotif] = useState(false);
+
   const handleLogout = () => {
     axios
       .get("http://localhost:5000/api/auth/logout")
-      .then((res) => {
+      .then(() => {
         location.reload(true);
       })
       .catch((err) => console.log(err));
   };
+
+  const handleNotifClick = () => {
+    setShowNotif(!showNotif);
+    setNotifCount(0); // Mark notifications as read
+  };
+
   return (
     <div
-      className="px-4 p-1 w-100 d-flex  justify-content-end bg-white"
+      className="px-4 p-1 w-100 d-flex justify-content-end align-items-center bg-white"
       style={{ height: "4rem" }}
     >
-      <>
-        <Dropdown>
-          <Dropdown.Toggle
-            bsPrefix="custom-dropdown-toggle"
-            className="w-100 py-2 px-5 mt-2 d-flex justify-content-center align-items-center"
-            id="dropdown-basic"
-            style={{
-              backgroundColor: "var(--main-color)",
-            }}
-          >
-            <p
-              className="m-0 fs-7"
-              style={{ color: "var(--secondMain-color)" }}
+      {/* Notification Dropdown */}
+      <Dropdown show={showNotif} onToggle={setShowNotif}>
+        <Dropdown.Toggle
+          as={Button}
+          variant="transparent"
+          className="position-relative me-3"
+          onClick={handleNotifClick}
+          bsPrefix="custom-toggle"
+        >
+          <IoNotificationsOutline size={24} />
+          {notifCount > 0 && (
+            <span
+              className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"
+              style={{ fontSize: "0.75rem" }}
             >
-              {user.firstName}
-            </p>
-          </Dropdown.Toggle>
-          <Dropdown.Menu>
-            <Dropdown.Item className="btn btn-close-white d-flex align-items-center text-center justify-content-center">
-              <Link
-                to="/ProfileSetup"
-                className="text-decoration-none text-dark"
-              >
-                Profile
-              </Link>
-            </Dropdown.Item>
-            <Dropdown.Item className="btn btn-close-white no-hover">
-              <button
-                className="btn btn-light w-100 text-center border-0 bg-transparent"
-                onClick={handleLogout}
-              >
-                Logout
-              </button>
-            </Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-      </>
+              {notifCount}
+            </span>
+          )}
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu className="p-2" style={{ minWidth: "250px" }}>
+          <Dropdown.ItemText className="fw-bold">
+            Notifications
+          </Dropdown.ItemText>
+          <Dropdown.Divider />
+          <Dropdown.Item href="#">✅ Your request was approved</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+
+      {/* User Dropdown */}
+      <Dropdown>
+        <Dropdown.Toggle
+          bsPrefix="custom-dropdown-toggle"
+          className="py-2 px-5 d-flex justify-content-center align-items-center"
+          id="dropdown-basic"
+          style={{
+            backgroundColor: "var(--main-color)",
+          }}
+        >
+          <p className="m-0 fs-7" style={{ color: "var(--secondMain-color)" }}>
+            {user?.firstName}
+          </p>
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+          <Dropdown.Item className="d-flex align-items-center justify-content-center">
+            <Link to="/ProfileSetup" className="text-decoration-none text-dark">
+              Profile
+            </Link>
+          </Dropdown.Item>
+          <Dropdown.Item>
+            <button
+              className="btn btn-light w-100 text-center border-0 bg-transparent"
+              onClick={handleLogout}
+            >
+              Logout
+            </button>
+          </Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
     </div>
   );
 };
